3d4f63e1c3dffe47d4e2d319562d084834914a6d
jheizer wandel <jheizer@hotmail.com>
2016-03-23 11:12:51 -0300

    alterações

--- controle-atendimento.php
+++ controle-atendimento.php
@@ -364,7 +364,7 @@
 								</li>
 								<li  ng-click="loadPagamentosPaciente();showButtonSalvar = false;">
 									<a  href="#pagamentos" data-toggle="tab">
-										<i class="fa fa-list-alt"></i> pagamentos
+										<i class="fa fa-list-alt"></i> Pagamentos
 									</a>
 								</li>
 								<li  ng-click="getItensVenda();showButtonSalvar = false;">
